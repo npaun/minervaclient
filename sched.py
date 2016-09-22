a023466d9f877543be8290487dedd546c56d9027
Nicholas Paun <np@icebergsystems.ca>
2016-09-22 17:59:44 -0400

    Embarassing mistake

--- sched.py
+++ sched.py
@@ -4,7 +4,7 @@ import sched_parse,sched_timetable,sched_ics
 
 def course_details(term,report = 'default',visual = False,calendar = False,conflicts_only = False,no_conflicts = False):
 	minerva_login()
-	mineva_reg_menu()
+	minerva_reg_menu()
 	minerva_get('bwskfshd.P_CrseSchdDetl')
 	r = minerva_post('bwskfshd.P_CrseSchdDetl',{'term_in': term})
 
