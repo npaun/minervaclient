b29a206e80306fbe955abbb06e0fc7ebe351d2c8
Nicholas Paun <nicholas.paun@mail.mcgill.ca>
2017-01-22 14:13:40 -0500

    Blast a few more building abbreviations that might possibly exist.

--- minerva_common.py
+++ minerva_common.py
@@ -88,7 +88,12 @@ def get_type_abbrev(type):
 
 # Doesn't really do much. Just tries a few tricks to shorten the names of buildings
 def get_bldg_abbrev(location):
-        subs = {'Building': '', 'Hall': '', 'Pavilion': '','House': '','Centre': '','Biology': 'Bio.','Chemistry': 'Chem.','Physics': 'Phys.', 'Engineering': 'Eng.','Anatomy': 'Anat.', 'Dentistry': 'Dent.','Medical': 'Med.', 'Life Sciences': 'Life. Sc.'}
+        subs = {
+                'Building': '', 'Hall': '', 'Pavilion': '','House': '','Centre': '', 'Complex': '',
+                'Library': 'Lib.', 'Laboratory': 'Lab.',
+                'Biology': 'Bio.', 'Chemistry': 'Chem.',' Physics': 'Phys.', 'Engineering': 'Eng.', 'Anatomy': 'Anat.', 'Dentistry': 'Dent.', 'Medical': 'Med.', 'Life Sciences': 'Life Sc.'
+        }
+
 	for sub in subs:
 		location = location.replace(sub,subs[sub])
 
