acf28bee632d62ee0e1ba314cdcfcf233560c131
Nicholas Paun <np@icebergsystems.ca>
2017-05-08 14:47:15 -0400

    Remove <sample> markers from credentials file, as it is confusing for people not used to that convention.

--- credentials.py
+++ credentials.py
@@ -1,4 +1,6 @@
 ### Copy this file to credentials_local.py and edit the settings
-id = '<McGill ID here>'
-pin = '<Minerva PIN here>'
+### ONLY EDIT THIS FILE WITH A REAL TEXT EDITOR (No TextEdit or Notepad, ever!)
+
+id = 'McGill ID here'
+pin = 'Minerva PIN here'
 always_dry_run = False
